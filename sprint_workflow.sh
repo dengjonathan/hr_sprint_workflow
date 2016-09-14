5ed5a70811531c3435467aa4c0b60e0cfc626e4d
jonathan.deng <dengjonathan@gmail.com>
2016-09-14 16:39:44 -0400

    updated variables with FILL me in

--- sprint_workflow.sh
+++ sprint_workflow.sh
@@ -1,7 +1,8 @@
-downloadDir='~/Downloads' # where items are downloaded by default; in OSX it's ~/Downloads
-targetDir='~/Google Drive/Programming/hack_reactor' # the directory you want to store your sprint folders
-remoteName=myFork # ALERT!! change myFork to the name you want your remote to be
-githubURL='https://github.com/dengjonathan/2016-09-sprints' # and the github url to the url of your sprints repo (Should be github.com/YOUR NAME/ 2016-09-sprints)
+# make sure you keep the quotation marks in case some of your directories have spaces/ special characters
+downloadDir='FILL_ME_IN' # where items are downloaded by default; in OSX it's ~/Downloads
+targetDir='FILL_ME_IN' # the directory you want to store your sprint folders
+remoteName='FILL_ME_IN' # ALERT!! change myFork to the name you want your remote to be
+githubURL='FILL_ME_IN' # and the github url to the url of your sprints repo (Should be github.com/YOUR NAME/ 2016-09-sprints)
 
 # this works in OSX - windows might have to replace the unzip command with some other tool
 unzip -n $downloadDir/course.$1.exercise
